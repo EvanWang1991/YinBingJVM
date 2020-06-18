5aa9f9a38ea913aac6944004480eb22a9d0e0b43
jixiang.wang <jixiang.wang@openjawtech.com>
2020-06-18 17:58:05 +0800

    update README.md and add some emample code

---
+++ src/main/java/com/yinbing/jvm/TestDynamicLoad.java
@@ -0,0 +1,33 @@
+package com.yinbing.jvm;
+
+public class TestDynamicLoad {
+    static {
+        System.out.println("*************load TestDynamicLoad************");
+    }
+
+    public static void main(String[] args) {
+        new A();
+        System.out.println("*************load test************");
+        B b = null;  //B不会加载，除非这里执行 new B()
+    }
+}
+
+class A {
+    static {
+        System.out.println("*************load A************");
+    }
+
+    public A() {
+        System.out.println("*************initial A************");
+    }
+}
+
+class B {
+    static {
+        System.out.println("*************load B************");
+    }
+
+    public B() {
+        System.out.println("*************initial B************");
+    }
+}
