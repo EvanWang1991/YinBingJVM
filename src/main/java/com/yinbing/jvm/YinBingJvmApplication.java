138bfa4e5ad78e981ba4191a6ba7ddfce63703ca
jixiang.wang <jixiang.wang@openjawtech.com>
2020-06-18 15:52:27 +0800

    Initial commit

---
+++ src/main/java/com/yinbing/jvm/YinBingJvmApplication.java
@@ -0,0 +1,13 @@
+package com.yinbing.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinBingJvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinBingJvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yinbing/jvm/YinBingJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinbing.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinBingJvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
